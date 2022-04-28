3dbfa6f57d5a28f80bf9161bda7a88e1194046b2
VolodymyrKavun <92682811+VolodymyrKavun@users.noreply.github.com>
2022-04-28 10:32:49 +0300

    last-fixed-task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -15,8 +15,7 @@ const galleryMarkup = createCardsImageMarkup(galleryItems);
 
 /**
  * Функція створення розмітки елементів галереї
- * @param {element} card
- * @returns {}
+ *
  */
 function createCardsImageMarkup(card) {
   return card
@@ -36,73 +35,51 @@ function createCardsImageMarkup(card) {
 }
 
 /**
- * Додавання дозмітки в DOM
+ * Додавання розмітки в DOM
  */
 refs.gallery.insertAdjacentHTML("beforeend", galleryMarkup);
 
 /**
- *  Функція заборони переходу на силку та одночасне вішання Функції з картинкою
- * @param {event} event
+ * Модалка
  */
-function onImagePreventClick(event) {
+
+const instance = basicLightbox.create(
+  `<img class="modal-img" src="">`,
+  {
+    onShow: (instance) => {
+      window.addEventListener("keydown", onEscClick);
+    },
+  },
+  {
+    onClose: (instance) => {
+      window.removeEventListener("keydown", onEscClick);
+    },
+  }
+);
+
+// Функція відкривання модалки
+
+function onImgClick(event) {
   event.preventDefault();
-
-  event.target.addEventListener("click", createImageOnModalLightBox);
-}
-
-/**
- * Вішання слухача по кліку
- */
-refs.gallery.addEventListener("click", onImagePreventClick);
-
-/**
- * BackDrop
- */
-const modal = document.createElement("div");
-modal.id = "lightbox";
-refs.gallery.after(modal);
-
-/**
- * Функція додавання картинки на модалку
- * @param {event} event
- */
-function createImageOnModalLightBox(event) {
-  modal.classList.add("active");
-  window.addEventListener("keydown", onKeyToClose);
-
-  /**
-   * Картинка
-   */
-  const img = document.createElement("img");
-  img.src = event.target.dataset.source;
-
-  if (modal.firstChild) {
-    modal.removeChild(modal.firstChild);
+  if (event.target.nodeName !== "IMG") {
+    return;
   }
 
-  modal.append(img);
+  instance.element().querySelector(".modal-img").src =
+    event.target.dataset.source;
+
+  instance.show();
 }
 
-/**
- * Вішання на слухача функції закривання
- */
-modal.addEventListener("click", onClickModalToClose);
+// Функція закривання кнопкою Escape
 
-/**
- * Функція закривання модалки
- * @param {event} event
- */
-function onClickModalToClose(event) {
-  modal.classList.remove("active");
-  window.removeEventListener("keydown", onKeyToClose);
-}
-
-/**
- * Функція закривання модалки кнопкою
- * @param {event} event
- */
-function onKeyToClose(event) {
-  if (event.code === "Escape") {
-    onClickModalToClose();
+function onEscClick(event) {
+  if (event.key === "Escape") {
+    instance.close();
+    return;
   }
 }
+
+// Вішання слухача
+
+refs.gallery.addEventListener("click", onImgClick);
